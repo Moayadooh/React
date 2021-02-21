ed1b18e1162a5f002d8b55179e7a8b5d7cab0b78
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-02-21 21:05:34 +0400

    ***

---
+++ learn-antdesign/src/Modal.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Modal, Button } from 'antd';
+
+function ModalBox() {
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    return (
+        <div className="modal">
+            <>
+                <Button type="primary" onClick={showModal}>Open Modal</Button>
+                <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
+            </>
+        </div>
+    );
+}
+
+export default ModalBox;
--- learn-antdesign/src/index.js
+++ learn-antdesign/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import SliderDemo from './Slider';
+import TablePagination from './Table';
+import Modal from './Modal';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <SliderDemo /> */}
+    {/* <TablePagination /> */}
+    <Modal />
   </React.StrictMode>,
   document.getElementById('root')
 );
