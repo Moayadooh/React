2b04703d671adde70c995d83726ada96fb0dbb60
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-02-28 22:16:43 +0400

    ***

--- react-redux-lessons/src/redux/store.js
+++ react-redux-lessons/src/redux/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 //import cakeReducer from './cake/cakeReducer';
-//import logger from 'redux-logger';
-//import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 
 //const store = createStore(cakeReducer)
-const store = createStore(rootReducer)
-//const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
+//const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
 
 export default store
