621e01f6810d29664facbcf4c4f008696e59a854
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-03-05 21:06:36 +0400

    ***

---
+++ react-projects-master/1-birthday-reminder/setup/src/List.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+
+  // return (
+  //   <>
+  //     {people.map((person) => (
+  //       <article key={person.id} className="person">
+  //           <img src={person.image} alt={person.name} />
+  //           <div>
+  //             <h4>{person.name}</h4>
+  //             <p>{person.age} years</p>
+  //           </div>
+  //         </article>
+  //     ))}
+  //   </>
+  // );
+
+};
+
+export default List;
